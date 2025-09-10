2e109efacf862f254e3b8df9e4c22e908adbdf8c
Samir Asif <74524705+Samir529@users.noreply.github.com>
2025-09-11 01:12:33 +0600

    Update settings.py

--- Billboard-Advertisement-System/Billboard_Advertisement/Billboard_Advertisement/settings.py
+++ Billboard-Advertisement-System/Billboard_Advertisement/Billboard_Advertisement/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3w7i)%8bs1kd)rhz6&ck4rm_7)q2umdx!oajrpc@y3iu+g+_)$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
