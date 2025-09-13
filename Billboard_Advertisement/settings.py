75588e00f71d8c4ad21f3595c789cf51b0cc38b4
Samir529 <samirasif529@gmail.com>
2025-09-13 21:37:33 +0600

    commit

--- Billboard_Advertisement/settings.py
+++ Billboard_Advertisement/settings.py
@@ -22,13 +22,13 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    'valuetech.onrender.com'
+    'billboard-advertisement-system.onrender.com'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "https://valuetech.onrender.com",
+    "https://billboard-advertisement-system.onrender.com",
 ]
 
 # Application definition
